62431a1e2d7fd61d31f040db657baf0e236063e9
derkanw <60828039+derkanw@users.noreply.github.com>
2021-07-12 16:47:45 +0300

    Add Slider

--- New Year's eve/Assets/Scripts/childParams.cs
+++ New Year's eve/Assets/Scripts/childParams.cs
@@ -1,16 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ChildParams : MonoBehaviour
 {
     public int dissatisfaction, power;
     public float speed;
+    public GameObject barPrefab;
+    private GameObject bar;
 
     void OnTriggerEnter(Collider collider)
     {
         if (collider.name == "Gift 3(Clone)")
+        {
             dissatisfaction -= collider.gameObject.GetComponent<GiftParams>().firePower;
+            bar.gameObject.transform.GetChild(0).GetComponent<Slider>().value = dissatisfaction;
+        }
         if (collider.name == "house")
         {
             print("Game Over!");
@@ -23,6 +29,10 @@ public class ChildParams : MonoBehaviour
     void Start()
     {
         GetComponent<Rigidbody>().velocity = new Vector3(-1, 0, 0) * speed;
+        bar = Instantiate(barPrefab, new Vector3(transform.position.x, transform.position.y + 1, transform.position.z), Quaternion.identity);
+        print(bar.name);
+        bar.gameObject.transform.GetChild(0).GetComponent<Slider>().maxValue = dissatisfaction;
+        bar.gameObject.transform.GetChild(0).GetComponent<Slider>().value = dissatisfaction;
     }
 
     void Update()
